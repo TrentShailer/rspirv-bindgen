ca34f43c51c2ec494bdcb782780e0d454b5d6d72
Trent Shailer <trent.shailer@gmail.com>
2025-03-08 12:26:04 +1300

    Basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,43 @@
-fn main() {}
+//! # spirv-bindgen
+//! CLI to generate Rust bindings for Spir-V shaders.
+//!
+
+use std::{fs, path::PathBuf};
+
+use clap::{Parser, command};
+use spirv_bindgen::Spirv;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// The Spir-V file to generate bindings for.
+    source: PathBuf,
+
+    /// The output file to write the bindings to.
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let source_bytes = fs::read(&cli.source).unwrap();
+
+    let spirv = Spirv::try_from_bytes(&source_bytes);
+    let binding_string = spirv.pretty_string();
+
+    if let Some(output) = cli.output.as_ref() {
+        if output.exists() && !output.is_file() {
+            println!("Output must be a file.");
+            return;
+        }
+
+        if let Some(parent) = output.parent() {
+            fs::create_dir_all(parent).unwrap();
+        }
+
+        fs::write(output, binding_string).unwrap();
+    } else {
+        println!("{binding_string}");
+    }
+}
--- tests/generate_bindings.rs
+++ tests/generate_bindings.rs
@@ -5,36 +5,25 @@ use spirv_bindgen::Spirv;
 
 #[test]
 fn generate_bindings_spec_constants() {
-    let spirv = Spirv::try_from_bytes(
-        "spec_constants",
-        include_bytes!("./data/spv/spec_constants.spv").as_slice(),
-    );
+    let spirv = Spirv::try_from_bytes(include_bytes!("./data/spv/spec_constants.spv").as_slice());
     println!("{}", spirv.pretty_string());
 }
 
 #[test]
 fn generate_bindings_render_capture() {
-    let spirv = Spirv::try_from_bytes(
-        "render_capture",
-        include_bytes!("./data/spv/render_capture.spv").as_slice(),
-    );
+    let spirv = Spirv::try_from_bytes(include_bytes!("./data/spv/render_capture.spv").as_slice());
     println!("{}", spirv.pretty_string());
 }
 
 #[test]
 fn generate_bindings_render_line() {
-    let spirv = Spirv::try_from_bytes(
-        "render_line",
-        include_bytes!("./data/spv/render_line.spv").as_slice(),
-    );
+    let spirv = Spirv::try_from_bytes(include_bytes!("./data/spv/render_line.spv").as_slice());
     println!("{}", spirv.pretty_string());
 }
 
 #[test]
 fn generate_bindings_maximum_reduction() {
-    let spirv = Spirv::try_from_bytes(
-        "maximum_reduction",
-        include_bytes!("./data/spv/maximum_reduction.spv").as_slice(),
-    );
+    let spirv =
+        Spirv::try_from_bytes(include_bytes!("./data/spv/maximum_reduction.spv").as_slice());
     println!("{}", spirv.pretty_string());
 }
